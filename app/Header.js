45f6b1a5a8eaaa99c399c21813232f866ae9d1dd
Moreno Dore <moreno.dore@tuta.io>
2023-10-12 08:20:04 +0200

    Fix cart count even if it worked..

--- app/Header.js
+++ app/Header.js
@@ -1,6 +1,25 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
 import NumberOfItems from './NumberOfItems';
+import { getCookie } from '../utilities/cookies';
+import { parseJson } from '../utilities/json';
+
+export async function numberOfItems() {
+  const currentCartCookie = getCookie('cart');
+  let count = 0;
+
+  const currentCart = await (currentCartCookie
+    ? parseJson(currentCartCookie)
+    : []);
+
+  console.log(currentCart);
+
+  for (let i = 0; i < currentCart.length; i++) {
+    count += currentCart[i].quantity || 0;
+  }
+
+  return <span> Your Items: {count}</span>;
+}
 
 export default function Header() {
   return (
@@ -23,7 +42,8 @@ export default function Header() {
         </div>
         <div className={styles.itemsCount}>
           <NumberOfItems data-test-id="cart-count">
-            {NumberOfItems}
+            {numberOfItems}
+            console.log({numberOfItems})
           </NumberOfItems>
         </div>
       </div>
