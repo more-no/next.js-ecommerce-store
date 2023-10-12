ded13aad533f98b60ef67a8dc50d61a3b98e1bf2
Moreno Dore <moreno.dore@tuta.io>
2023-10-12 08:31:13 +0200

    That was useless. Back to normal.

--- app/Header.js
+++ app/Header.js
@@ -1,25 +1,6 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
 import NumberOfItems from './NumberOfItems';
-import { getCookie } from '../utilities/cookies';
-import { parseJson } from '../utilities/json';
-
-export async function numberOfItems() {
-  const currentCartCookie = getCookie('cart');
-  let count = 0;
-
-  const currentCart = await (currentCartCookie
-    ? parseJson(currentCartCookie)
-    : []);
-
-  console.log(currentCart);
-
-  for (let i = 0; i < currentCart.length; i++) {
-    count += currentCart[i].quantity || 0;
-  }
-
-  return <span> Your Items: {count}</span>;
-}
 
 export default function Header() {
   return (
@@ -42,8 +23,7 @@ export default function Header() {
         </div>
         <div className={styles.itemsCount}>
           <NumberOfItems data-test-id="cart-count">
-            {numberOfItems}
-            console.log({numberOfItems})
+            {NumberOfItems}
           </NumberOfItems>
         </div>
       </div>
--- app/NumberOfItems.js
+++ app/NumberOfItems.js
@@ -1,19 +1,19 @@
-// import { getCookie } from '../utilities/cookies';
-// import { parseJson } from '../utilities/json';
+import { getCookie } from '../utilities/cookies';
+import { parseJson } from '../utilities/json';
 
-// export default async function NumberOfItems() {
-//   const currentCartCookie = getCookie('cart');
-//   let count = 0;
+export default async function NumberOfItems() {
+  const currentCartCookie = getCookie('cart');
+  let count = 0;
 
-//   const currentCart = await (currentCartCookie
-//     ? parseJson(currentCartCookie)
-//     : []);
+  const currentCart = await (currentCartCookie
+    ? parseJson(currentCartCookie)
+    : []);
 
-//   console.log(currentCart);
+  console.log(currentCart);
 
-//   for (let i = 0; i < currentCart.length; i++) {
-//     count += currentCart[i].quantity || 0;
-//   }
+  for (let i = 0; i < currentCart.length; i++) {
+    count += currentCart[i].quantity || 0;
+  }
 
-//   return <span> Your Items: {count}</span>;
-// }
+  return <span> Your Items: {count}</span>;
+}
