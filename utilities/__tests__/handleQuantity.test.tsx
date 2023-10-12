be65465ce53d5da207537aae4d870c0b542cfb41
Moreno Dore <moreno.dore@tuta.io>
2023-10-12 17:20:50 +0200

    Fix Jest test

--- utilities/__tests__/handleQuantity.test.tsx
+++ utilities/__tests__/handleQuantity.test.tsx
@@ -4,7 +4,7 @@ import { handleQuantity } from '../../app/products/[productId]/handleQuantity';
 //  JEST Unit: Test function for updating quantity in item of cookie
 //  (eg. adding an item to the cart that already exists)
 
-test('', () => {
+test('Test function for updating quantity in item of cookie', () => {
   expect(handleQuantity(3, 7)).toBe(10);
   expect(handleQuantity(3, 4)).toBe(7);
   expect(handleQuantity(5, 3)).toBe(8);
